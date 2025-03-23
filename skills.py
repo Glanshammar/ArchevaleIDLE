17ac87d9205921ee29fafd7627a05447537342e7
Andreas Johansson <glanshammar70@gmail.com>
2025-03-23 20:35:43 +0100

    Added skills

--- skills.py
+++ skills.py
@@ -2,8 +2,8 @@ class Skill:
     def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.level = 0
-        self.xp = 0
+        self.level : int = 0
+        self.xp : int = 0
 
     def gain_xp(self, amount):
         self.xp += amount
@@ -19,3 +19,23 @@ class Skill:
 class Woodcutting(Skill):
     def __init__(self):
         super().__init__("Woodcutting", "A skill for cutting trees.")
+        self.level : int = 120
+        self.xp : int = 200000000
+
+class Fishing(Skill):
+    def __init__(self):
+        super().__init__("Fishing", "A skill for catching fish.")
+        self.level : int = 99
+        self.xp : int = 13034431
+
+class Strength(Skill):
+    def __init__(self):
+        super().__init__("Strength", "Determines your ability to lift heavy object and dealing physical damage.")
+
+class Magic(Skill):
+    def __init__(self):
+        super().__init__("Fishing", "Create magical items and make enchantments, and cast magical spells both offensive and defensive.")
+
+class Archery(Skill):
+    def __init__(self):
+        super().__init__("Fishing", "A combat skill for bows, crossbows and other projectile weaponry.")

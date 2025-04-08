985573eb481e78a92bc00749375b1a7dc492d70b
Andreas Johansson <glanshammar70@gmail.com>
2025-04-08 16:36:35 +0200

    Added music

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame
+from pygame import mixer
 import sys
 import os
 from Functionality import *
 from globals import *
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, current_dir)
-
 pygame.init()
+mixer.init()
+mixer.music.load('/home/mondus/Music/music.mp3')
+mixer.music.play(-1)
 
 screen_width = 1280
 screen_height = 720
@@ -32,13 +33,13 @@ while True:
                 player.woodcutting.gain_xp(xp_gain)
                 print(f"Added {xp_gain} XP to Woodcutting! Current XP: {player.woodcutting.xp}")
             if event.key == pygame.K_0:
-                gamebg.set_show('shop')
+                gamebg.set_background('shop')
             if event.key == pygame.K_1:
-                gamebg.set_show('city')
+                gamebg.set_background('city')
         
-        if gamebg.show_shop:
+        if gamebg.shop:
             background = 'Media/shop1.jpeg'
-        if gamebg.show_city:
+        if gamebg.city:
             background = 'Media/city1.jpeg'
 
     DrawBackground(screen=screen, background_path=background)

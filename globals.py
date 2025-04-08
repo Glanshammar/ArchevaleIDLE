a5da75af1ced6fb4802deb321efabd8f8cf1e028
Andreas Johansson <glanshammar70@gmail.com>
2025-04-08 16:36:09 +0200

    Refactored class

--- globals.py
+++ globals.py
@@ -1,17 +1,17 @@
 class GameBackground:
     def __init__(self):
-        self.show_shop = False
-        self.show_city = False
-        self.show_forest = False
+        self.shop = False
+        self.city = False
+        self.forest = False
 
-    def set_show(self, target):
-        self.show_shop = False
-        self.show_city = False
-        self.show_forest = False
+    def set_background(self, target):
+        self.shop = False
+        self.city = False
+        self.forest = False
         
         if target == 'shop':
-            self.show_shop = True
+            self.shop = True
         elif target == 'city':
-            self.show_city = True
+            self.city = True
         elif target == 'forest':
-            self.show_forest = True
+            self.forest = True
